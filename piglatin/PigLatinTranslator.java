4ef4110b680ab21c7d8e624d1e1ddeaf23b72cbc
James-Moitra <jamesmoitra1@gmail.com>
2025-10-22 21:46:56 +0000

    created method

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -4,43 +4,10 @@ public class PigLatinTranslator {
     public static Book translate(Book input) {
         Book translatedBook = new Book();
 
-        // TODO: Add code here to populate translatedBook with a translation of the
-        // input book.
-        // Curent do-nothing code will return an empty book.
-        // Your code will need to call translate(String input) many times.
+        // Assuming Book has a method to get and set text
+        String[] lines = input.getText().split("\n");
+        for (String line : lines) {
+            translatedBook.addText(translate(line) + "\n");
+        }
 
-        return translatedBook;
-    }
-
-    public static String translate(String input) {
-        System.out.println("  -> translate('" + input + "')");
-
-        String result = "";
-
-        // TODO: translate a string input, store in result.
-        // The input to this function could be any English string.
-        // It may be made up of many words.
-        // This method must call translateWord once for each word in the string.
-        result = translateWord(input);
-
-        return result;
-    }
-
-    private static String translateWord(String input) {
-        System.out.println("  -> translateWord('" + input + "')");
-
-        String result = "";
-
-        // TODO: Replace this code to correctly translate a single word.
-        // Start here first!
-        // This is the first place to work.
-        result = input; // delete this line
-
-        return result;
-    }
-
-    // Add additonal private methods here.
-    // For example, I had one like this:
-    // private static String capitalizeFirstLetter(String input)
-
-}
+    
