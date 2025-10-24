e144a41c8d6e7ac1c872fd09b70fc650daaeb9cd
James-Moitra <jamesmoitra1@gmail.com>
2025-10-24 21:06:09 +0000

    code passes 10/12 tests, need to improve code

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -14,4 +14,90 @@ public class PigLatinTranslator {
 
         return translatedBook;
     }
-    
+
+    public static String translate(String input) {
+        System.out.println("  -> translate('" + input + "')");
+
+        Scanner scanner = new Scanner(input);
+        StringBuilder result = new StringBuilder();
+
+        while (scanner.hasNext()) {
+            String word = scanner.next();
+            result.append(translateWord(word)).append(" ");
+        }
+
+        scanner.close();
+        return result.toString().trim();
+    }
+
+    private static String translateWord(String input) {
+        System.out.println("  -> translateWord('" + input + "')");
+
+        // Check for punctuation
+        String punctuation = "";
+        if (!Character.isLetter(input.charAt(input.length() - 1))) {
+            punctuation = input.substring(input.length() - 1);
+            input = input.substring(0, input.length() - 1);
+        }
+
+        // Preserve capitalization
+        boolean capitalize = Character.isUpperCase(input.charAt(0));
+        boolean allCaps = isAllCaps(input);
+        input = input.toLowerCase();
+
+        // Check for hyphenated words
+        if (input.contains("-")) {
+            String[] parts = input.split("-");
+            String translated = translateWord(parts[0]) + "-" + translateWord(parts[1]);
+            return restoreCapitalization(translated, capitalize, allCaps) + punctuation;
+        }
+
+        // Translate word
+        int vowelIndex = findFirstVowelIndex(input);
+        String result;
+        if (vowelIndex == 0) {
+            result = input + "ay";
+        } else {
+            result = input.substring(vowelIndex) + input.substring(0, vowelIndex) + "ay";
+        }
+
+        return restoreCapitalization(result, capitalize, allCaps) + punctuation;
+    }
+
+    private static int findFirstVowelIndex(String word) {
+        String vowels = "aeiou";
+        for (int i = 0; i < word.length(); i++) {
+            if (vowels.indexOf(word.charAt(i)) != -1) {
+                return i;
+            }
+        }
+        return 0; // If no vowel is found, move the entire word
+    }
+
+    private static boolean isAllCaps(String word) {
+        for (char c : word.toCharArray()) {
+            if (Character.isLetter(c) && !Character.isUpperCase(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static String restoreCapitalization(String word, boolean capitalize, boolean allCaps) {
+        if (allCaps) {
+            return word.toUpperCase();
+        } else if (capitalize) {
+            return word.substring(0, 1).toUpperCase() + word.substring(1);
+        } else {
+            return word;
+        }
+    }
+}
+
+
+
+
+
+
+
+
