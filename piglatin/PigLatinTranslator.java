f1da8ae29ccc073c00dbc0a85a57bfac5ea185d5
James-Moitra <jamesmoitra1@gmail.com>
2025-10-28 20:52:08 +0000

    finished project

--- piglatin/PigLatinTranslator.java
+++ piglatin/PigLatinTranslator.java
@@ -5,11 +5,11 @@ import java.util.Scanner;
 public class PigLatinTranslator {
     public static Book translate(Book input) {
         Book translatedBook = new Book();
+        translatedBook.setTitle(input.getTitle());
 
-        // Assuming Book has a method to get and set text
         String[] lines = input.getText().split("\n");
         for (String line : lines) {
-            translatedBook.addText(translate(replaceDashesWithSpaces(line)) + "\n");
+            translatedBook.addText(translate(replaceDashesWithSpaces(line)));
         }
 
         return translatedBook;
