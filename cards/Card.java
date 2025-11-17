bdbebaee3c96109fe500b7fc4072dc261c5c20e2
James-Moitra <jamesmoitra1@gmail.com>
2025-11-17 21:50:17 +0000

    card class method

--- cards/Card.java
+++ cards/Card.java
@@ -1,6 +1,22 @@
 package cards;
 
 public class Card {
+    private int suit;
+    private int value;
 
+    public Card(int suit, int value) {
+        this.suit = Math.max(0, Math.min(suit, 3));
+        this.value = Math.max(0, Math.min(value, 12));
+    }
 
+    public int getValue() {
+        return value;
+    }
+
+    @Override
+    public String toString() {
+        String[] suits = {"♦", "♣", "♥", "♠"};
+        String[] values = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        return suits[suit] + values[value];
+    }
 }
