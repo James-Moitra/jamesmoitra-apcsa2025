a99a0d0163a4f5f44e0ad887859a7af1bd593965
James-Moitra <jamesmoitra1@gmail.com>
2026-02-03 22:07:53 +0000

    finished selection sort

--- sorting/SelectionSort.java
+++ sorting/SelectionSort.java
@@ -4,5 +4,17 @@ public class SelectionSort implements Sorter {
 
     public void sort(int[] input) {
         System.out.println("Selection Sort!!!");
+        for (int start = 0; start < input.length; start++) {
+            int smallest = start;
+            for (int i = start; i < input.length; i++) {
+                if (input[i] < input[smallest]) {
+                    smallest = i;
+
+                }
+            }
+            int temp = input[start];
+            input[start] = input[smallest];
+            input[smallest] = temp;
+        }
     }
 }
